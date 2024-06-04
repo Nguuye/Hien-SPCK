6f443839127c93eec6ee8d5c971419c0669775ff
whis102 <ngchicong868@gmail.com>
2024-06-04 23:37:06 +0700

    update

--- app.js
+++
@@ -1,1 +0,0 @@
-const UnsplashEI = document.getElementById('')
--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,12 @@
-
-  (function ($) {
   
+// Tạo hiệu ứng load lại trang
+(function ($) {
+
   "use strict";
 
     // PRE LOADER
     $(window).load(function(){
-      $('.preloader').fadeOut(1000); // set duration in brackets    
+      $('.preloader').fadeOut(1000);  
     });
 
     // CUSTOM LINK
